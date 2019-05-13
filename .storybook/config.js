e07d9ee0991e0eee5e42ac9e2873c3d97c78c255
Wen Ting Wang <chubberlisk@outlook.com>
2019-05-13 08:51:30 +0100

    Refactor to move const inside function

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,7 @@
 import { configure } from '@storybook/react';
 
-const req = require.context('../src/components', true, /\.stories\.js$/);
-
 function loadStories() {
+  const req = require.context('../src/components', true, /\.stories\.js$/);
   req.keys().forEach(filename => req(filename));
 }
 
